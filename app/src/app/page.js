2b01378a350db0d0c32e21b5998edf8eee5b1d0a
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-07-11 11:59:08 +0100

    chore: added shortcuts to component folders, added basic video component layout

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <></>;
+  return (
+    <main>
+      <section className="flex w-full">
+        <h3>Trending</h3>
+        <h3>Newest/Oldest</h3>
+        <h3>My Uploads</h3>
+      </section>
+      <section>
+        {/* our map — getVideos? */}
+        <Thumbnail />
+      </section>
+    </main>
+  );
 }
---
+++ app/src/components_server/Thumbnail.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Thumbnail(props) {
+  return (
+    <div>
+      {/* tags */}
+      <Image
+        src={"/placeholder_video.jpg"}
+        width={150}
+        height={100}
+        alt="Play-button"
+      />
+      <section className="flex items-center justify-evenly">
+        <div>
+          <p>{data.title}</p>
+          <p>{data.username}</p>
+        </div>
+        {/* <Vote/> */}
+      </section>
+    </div>
+  );
+}
