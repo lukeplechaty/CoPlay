52d1f6eb461b60df2f44ddd9bcb9c07c466c0c3e
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-07-11 16:09:00 +0100

    style: added theme colours (dark default) and typeface with css and tailwind variables

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -1,6 +1,19 @@
 import "./globals.css";
 import NavBar from "@/components_client/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
+import { MuseoModerno, Comfortaa } from "next/font/google";
+
+const moderno = MuseoModerno({
+  variable: "--moderno",
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const comfortaa = Comfortaa({
+  variable: "--comfortaa",
+  weight: "variable",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "CoPlay",
@@ -11,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${[comfortaa.variable, moderno.variable].join(" ")}`}>
           <NavBar />
           {children}
         </body>
