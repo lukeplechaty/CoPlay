fffd723d54a1d7b1beec1c37084525b37799192a
Ross Fletcher <rossfletcher108@gmail.com>
2025-07-11 13:38:06 +0100

    added navbar and css

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import NavBar from "@/components_client/Navbar";
 export const metadata = {
   title: "CoPlay",
   description: "Video play with frends",
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
