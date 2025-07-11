a688d03b99d49ca9a166bc46655b421ace070dad
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-07-11 13:24:15 +0100

    chore: added layout to votes component, added link to thumbnail component [optional chaining meant only for development]

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,9 +1,9 @@
 import Thumbnail from "=/Thumbnail";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-between h-full w-full">
-      <section className="flex w-full justify-evenly items-center">
+      <section className="flex w-full justify-evenly items-center mb-4">
         <h3>Trending</h3>
         <h3>Newest/Oldest</h3>
         <h3>My Uploads</h3>
--- app/src/components_server/Thumbnail.jsx
+++ app/src/components_server/Thumbnail.jsx
@@ -1,23 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
+import Votes from "£/Votes";
 
 export default function Thumbnail(props) {
   return (
     <div className="p-2 rounded-2xl bg-slate-800/50">
       {/* tags */}
-      <Image
-        src={"/placeholder_video.jpg"}
-        width={250}
-        height={200}
-        alt="Play-button"
-        className="rounded-lg"
-      />
+      <Link href={"/"}>
+        <Image
+          src={"/placeholder_video.jpg"}
+          width={350}
+          height={250}
+          alt="Play-button"
+          className="rounded-lg"
+        />
+      </Link>
+
       <section className="flex items-center justify-evenly">
         <div>
           <p className="font-bold text-lg">{props?.title || "Title"}</p>
           <p>{props?.username || "User"}</p>
         </div>
         <p>{props?.views || "0"} Views</p>
-        <Vote count={props.votes} />
+        <Votes count={props?.votes} />
       </section>
     </div>
   );
