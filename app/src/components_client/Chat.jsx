dd532c7394c3458a6bb3acd6ead02d846cc7a85b
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-07-14 09:52:35 +0100

    setup base of video page

---
+++ app/src/components_client/Chat.jsx
@@ -0,0 +1,12 @@
+import Chatbox from "./Chatbox";
+export default function Chat() {
+  return (
+    <section>
+      <div></div>
+      {
+        //? client or no
+      }
+      <Chatbox />
+    </section>
+  );
+}
