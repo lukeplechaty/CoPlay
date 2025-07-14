3c1b702de0ed8b70e7ecceb5312028258fbe6e45
BracketProto <69521768+OCSYT@users.noreply.github.com>
2025-07-14 09:38:33 +0100

    Seach bars and page pagnation
    Added search query logic in database and front-end along with modifications in db.js for limiting results and adding result offsets, useful for page pagination in future and optimisation

--- app/src/db.js
+++ app/src/db.js
@@ -27,7 +27,7 @@ export async function addUser(uuid, username) {
   }
 }
 
-export async function getVideos() {
+export async function getVideos(limit = 100, offset = 0) {
   try {
     const { rows } = await db.query(
       `
@@ -35,7 +35,9 @@ export async function getVideos() {
       FROM videos 
       JOIN video_tag_links ON videos.id = video_tag_links.video_id 
       JOIN tags ON tags.id = video_tag_links.tag_id 
-      GROUP BY videos.id`
+      GROUP BY videos.id
+      LIMIT $1 OFFSET $2`,
+      [limit, offset]
     );
     return rows;
   } catch (error) {
@@ -58,5 +60,32 @@ export async function getVideo(id) {
     return rows[0];
   } catch (error) {
     throw new Error(`geting one video error: ${error}`);
+  }
+}
+
+export async function searchVideos(searchTerm, limit = 100, offset = 0) {
+  try {
+    const { rows } = await db.query(
+      `
+      SELECT videos.id, videos.url, videos.title, videos.views, videos.votes,
+        JSONB_AGG(JSONB_BUILD_OBJECT('type', tags.type, 'value', tags.value)) AS tags
+      FROM videos
+      JOIN video_tag_links ON videos.id = video_tag_links.video_id
+      JOIN tags ON tags.id = video_tag_links.tag_id
+      WHERE LOWER(videos.title) LIKE LOWER($1)
+         OR videos.id IN (
+           SELECT video_tag_links.video_id
+           FROM video_tag_links
+           JOIN tags ON tags.id = video_tag_links.tag_id
+           WHERE LOWER(tags.value) LIKE LOWER($1)
+         )
+      GROUP BY videos.id
+      LIMIT $2 OFFSET $3
+      `,
+      [`%${searchTerm}%`, limit, offset]
+    );
+    return rows;
+  } catch (error) {
+    throw new Error(`searching videos error: ${error}`);
   }
 }
