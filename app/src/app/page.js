1d1b9e885f79629559e62d4efa601ba66b0fe918
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-07-17 11:00:01 +0100

    styling pt.2: root page

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -2,6 +2,7 @@ import Thumbnail from "=/Thumbnail";
 import { getUser, getVideos, searchVideos, getUserVideos } from "@/db";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
+import style from "./tab.module.css";
 
 export default async function HomePage({ searchParams }) {
   const searchData = await searchParams;
@@ -26,18 +27,40 @@ export default async function HomePage({ searchParams }) {
   return (
     <main className="flex flex-col items-center justify-between h-full w-full">
       <section className="flex w-full justify-evenly items-center mb-4">
-        <Link href="?sort=trend">Trending</Link>
+        <Link
+          href="?sort=trend"
+          className={`${style.tab} ${sort === "trend" ? style.active : ``}`}
+        >
+          Trending
+        </Link>
         {sort === "asc" ? (
-          <Link href="?sort=desc">Newest</Link>
+          <Link
+            href="?sort=desc"
+            className={`${style.tab} ${sort === "asc" ? style.active : ``}`}
+          >
+            Newest
+          </Link>
         ) : (
-          <Link href="?sort=asc">Oldest</Link>
+          <Link
+            href="?sort=asc"
+            className={`${style.tab} ${sort === "desc" ? style.active : ``}`}
+          >
+            Oldest
+          </Link>
         )}
-        {DBid > 0 ? <Link href={`?user=${DBid}`}>My Uploads</Link> : null}
+        {DBid > 0 ? (
+          <Link
+            href={`?user=${DBid}`}
+            className={`${style.tab} ${user ? style.active : ``}`}
+          >
+            My Uploads
+          </Link>
+        ) : null}
       </section>
       <section className="body">
-        {videoArray?.map((v) => <Thumbnail key={v.id} video={v} />) || (
-          <h2>Post a video and start a trend!</h2>
-        )}
+        {videoArray?.map((v) => (
+          <Thumbnail key={v.id} video={v} user={user} />
+        )) || <h2>Post a video and start a trend!</h2>}
       </section>
     </main>
   );
--- app/src/components_server/Thumbnail.jsx
+++ app/src/components_server/Thumbnail.jsx
@@ -14,7 +14,7 @@ function generateRoomId(length = 8) {
     .substring(2, 2 + length);
 }
 
-export default async function Thumbnail({ video }) {
+export default async function Thumbnail({ video, user }) {
   const grops = video.tags.reduce((result, obj) => {
     (result[obj.type] = result[obj.type] || []).push(obj);
     return result;
@@ -34,11 +34,15 @@ export default async function Thumbnail({ video }) {
   downvotes = counts.downvotes;
 
   return (
-    <div className={`${thumbnail.body} p-2 rounded-2xl bg-slate-800/50 m-3`}>
+    <div className={`${thumbnail.body} p-2 rounded-2xl m-3`}>
       <div className={style.tagBox}>
-        {Object.entries(grops).map((tag, index) => (
-          <Tag key={index} tagList={tag} />
-        ))}
+        {Object.entries(grops).length > 0 ? (
+          Object.entries(grops).map((tag, index) => (
+            <Tag key={index} tagList={tag} />
+          ))
+        ) : (
+          <p className="opacity-50">No Tags</p>
+        )}
       </div>
       <ThumbnailClient video={video} />
       <section className={thumbnail.infoBox}>
@@ -51,16 +55,28 @@ export default async function Thumbnail({ video }) {
           ) : null}
         </div>
         <div className={thumbnail.stats}>
-          <p>{video?.views || "0"} Views</p>
-          <Votes
-            video_id={video?.id}
-            initialUpvotes={upvotes}
-            initialDownvotes={downvotes}
-            initialUserVote={userVote}
-            userId={userId}
-          />
+          <div className="mt-2">
+            <Votes
+              video_id={video?.id}
+              initialUpvotes={upvotes}
+              initialDownvotes={downvotes}
+              initialUserVote={userVote}
+              userId={userId}
+            />
+          </div>
+
+          <p className="text-xs mr-1 opacity-75">{video?.views || "0"} Views</p>
         </div>
       </section>
+      {user ? (
+        <div className="w-full mt-1">
+          <button className="px-4 py-1 rounded-2xl bg-red-400 font-bold text-[#081221] w-full cursor-pointer hover:bg-red-500 active:bg-red-800 transition-colors">
+            Delete
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
